d41a740893f7ab62e112fb700f513b506bb96edc
Robby Gouveia <robert@Robbys-MacBook-Pro.local>
2025-06-16 21:37:54 -0700

    Fix AuthContext import extension in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { AuthProvider } from './contexts/AuthContext.js'
+import { AuthProvider } from './contexts/AuthContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
